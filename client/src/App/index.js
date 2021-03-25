d2dbd5aa9959fb7b1b506c23e298cafd820631a4
Osama-you <osama.w.younis@gmail.com>
2021-03-25 18:23:49 +0200

    install react-router-dom and Routing the app Relates #58

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,11 +1,48 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import 'antd/dist/antd.less';
+import {
+  HOME_PAGE,
+  LOGIN_PAGE,
+  ABOUT_US,
+  REGISTER_PAGE,
+  ORDERS_PAGE,
+  PROVIDER_PROFILE_PAGE,
+  PROVIDER_DASHBOARD_PAGE,
+} from '../Utils/routes.constant';
+
 import './style.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>service seeker</h1>
+      <Switch>
+        <Route exact path={HOME_PAGE}>
+          Home Page
+        </Route>
+        <Route exact path={ABOUT_US}>
+          About Us
+        </Route>
+        <Route exact path={LOGIN_PAGE}>
+          Login Page
+        </Route>
+        <Route exact path={REGISTER_PAGE}>
+          Register Page
+        </Route>
+        <Route exact path={ORDERS_PAGE}>
+          Orders Page
+        </Route>
+        <Route exact path={PROVIDER_PROFILE_PAGE}>
+          Provider Profile Page
+        </Route>
+        <Route exact path={PROVIDER_DASHBOARD_PAGE}>
+          Provider Dashboard Page
+        </Route>
+        <Route>Not Found 404</Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
