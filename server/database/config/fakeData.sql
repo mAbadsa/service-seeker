6606a0e824a258a725e5e7eed5fc7e4a09028279
Ahmed-aed <aeid767@gmail.com>
2021-03-25 20:00:40 +0200

    add provider table

--- server/database/config/fakeData.sql
+++ server/database/config/fakeData.sql
@@ -38,3 +38,103 @@ insert into user (id, full-name, email, password, mobile, location, avater, rule
 insert into user (id, full-name, email, password, mobile, location, avater, rule) values (28, 'Veronika', 'vprozillor@bluehost.com', '123456789', '985-520-1712', '2304 Anderson Alley', 'https://robohash.org/repudiandaeautducimus.png?size=50x50&set=set1', 'user');
 insert into user (id, full-name, email, password, mobile, location, avater, rule) values (29, 'Evan', 'etodarinis@dion.ne.jp', '123456789', '382-805-5548', '9355 Banding Circle', 'https://robohash.org/eiusutrerum.png?size=50x50&set=set1', 'user');
 insert into user (id, full-name, email, password, mobile, location, avater, rule) values (30, 'Leandra', 'lnezeyt@seesaa.net', '123456789', '261-332-9465', '1843 Farwell Trail', 'https://robohash.org/odioquiminima.png?size=50x50&set=set1', 'user');
+  
+
+  create table provider (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON UPDATE CASCADE  NOT NULL,
+  bio TEXT  NOT NULL,
+  price_houer FLOAT  NOT NULL,
+  availability boolean DEFAULT true,
+  rating INTEGER DEFAULT 5 
+  );
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (53, 1, 'Phasellus in felis. Donec semper sapien a libero. Nam dui.
+
+Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.
+
+Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.', '$4.59', true, 5);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (35, 2, 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
+
+Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', '$7.84', true, 3);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (35, 3, 'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.', '$1.28', true, 4);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (58, 4, 'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.
+
+Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', '$6.17', true, 3);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (44, 5, 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.
+
+Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', '$4.26', true, 3);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (55, 6, 'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.', '$0.38', true, 2);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (52, 7, 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.
+
+Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
+
+Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', '$5.29', true, 5);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (63, 8, 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', '$2.21', true, 4);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (63, 9, 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', '$2.87', true, 4);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (42, 10, 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.
+
+Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.
+
+Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.', '$0.70', true, 4);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (62, 11, 'Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.', '$1.35', true, 2);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (59, 12, 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.
+
+Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.
+
+Sed ante. Vivamus tortor. Duis mattis egestas metus.', '$2.88', true, 1);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (55, 13, 'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.', '$1.89', true, 3);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (58, 14, 'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.
+
+Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.
+
+Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.', '$4.50', true, 2);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (62, 15, 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
+
+Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', '$4.91', true, 1);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (40, 16, 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', '$6.44', true, 4);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (53, 17, 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+
+Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', '$9.73', true, 2);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (57, 18, 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.', '$3.74', true, 4);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (34, 19, 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
+
+Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', '$7.16', true, 2);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (48, 20, 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', '$8.53', true, 2);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (39, 21, 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.
+
+Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.
+
+Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', '$0.30', true, 5);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (56, 22, 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', '$8.79', true, 1);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (51, 23, 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
+
+Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.
+
+Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', '$1.27', true, 1);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (60, 24, 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.
+
+Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.', '$4.22', true, 3);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (42, 25, 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+
+In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', '$5.97', true, 2);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (49, 26, 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.
+
+Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', '$4.03', true, 3);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (51, 27, 'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.
+
+Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '$6.81', true, 2);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (45, 28, 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.
+
+Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', '$2.68', true, 5);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (45, 29, 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+
+Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.
+
+Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.', '$5.46', true, 2);
+insert into provider (id, user_id, bio, price_houer, availability, rating) values (51, 30, 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.', '$2.62', true, 4);
