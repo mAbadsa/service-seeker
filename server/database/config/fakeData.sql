5bc4a4dad83a99d77a81140d89bfa24963069253
Ahmed-aed <aeid767@gmail.com>
2021-03-25 19:58:07 +0200

    create table and insert random data related #59

---
+++ server/database/config/fakeData.sql
@@ -0,0 +1,40 @@
+create table user (
+	id INT,
+	full-name VARCHAR(50),
+	email VARCHAR(50),
+	password VARCHAR(50),
+	mobile VARCHAR(50),
+	location VARCHAR(50),
+	avater VARCHAR(50),
+	rule VARCHAR(50)
+);
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (1, 'Reinald', 'rhrishanok0@google.com', '123456789', '959-398-7264', '2878 Di Loreto Road', 'https://robohash.org/excepturisitdolores.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (2, 'Melicent', 'mllewelyn1@ucoz.com', '123456789', '124-136-6347', '0724 Kim Crossing', 'https://robohash.org/minimacupiditateipsam.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (3, 'Dugald', 'dstrotone2@umich.edu', '123456789', '697-350-0993', '76 Birchwood Trail', 'https://robohash.org/ipsaeiusvel.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (4, 'Shelli', 'slamzed3@gnu.org', '123456789', '429-847-1775', '12437 Menomonie Court', 'https://robohash.org/minusnihildolorum.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (5, 'Sharon', 'smacfall4@goo.gl', '123456789', '281-895-5337', '8893 Dayton Circle', 'https://robohash.org/dignissimositaquevoluptatem.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (6, 'Bronson', 'bdulinty5@mail.ru', '123456789', '474-512-6942', '6478 Doe Crossing Terrace', 'https://robohash.org/voluptatumiureexcepturi.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (7, 'Kirstin', 'kattril6@hud.gov', '123456789', '534-547-2846', '3 Sachs Center', 'https://robohash.org/nisimaioresexpedita.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (8, 'Ronalda', 'rangell7@wikipedia.org', '123456789', '328-102-1843', '313 Sycamore Place', 'https://robohash.org/officialaboriosamut.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (9, 'Nananne', 'nsaladino8@salon.com', '123456789', '281-407-4475', '204 Northview Drive', 'https://robohash.org/temporeharumlaborum.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (10, 'Abagael', 'amannakee9@who.int', '123456789', '704-267-6234', '3112 Comanche Drive', 'https://robohash.org/autoccaecatiquam.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (11, 'Vernice', 'vmacallana@illinois.edu', '123456789', '708-505-1451', '67473 Portage Drive', 'https://robohash.org/eaqueeosdebitis.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (12, 'Aarika', 'acrumpeb@amazon.co.uk', '123456789', '467-662-5544', '27285 Nancy Hill', 'https://robohash.org/undelaboriosamlaudantium.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (13, 'Adair', 'ahendrikc@quantcast.com', '123456789', '928-416-7298', '93 Vermont Alley', 'https://robohash.org/eligendidelenitidolorem.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (14, 'Gilemette', 'ggatesd@example.com', '123456789', '624-218-2137', '0 Hoepker Street', 'https://robohash.org/distinctiodolorumratione.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (15, 'Fernando', 'fjeannessone@desdev.cn', '123456789', '802-154-8157', '70 Boyd Point', 'https://robohash.org/quiaofficiaassumenda.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (16, 'Ardenia', 'awatersf@sciencedirect.com', '123456789', '712-257-9958', '69866 Carioca Junction', 'https://robohash.org/necessitatibusvelitfugiat.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (17, 'Kitty', 'krutiglianog@4shared.com', '123456789', '455-813-8382', '03770 Scott Hill', 'https://robohash.org/molestiaemodiid.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (18, 'Claudell', 'ckordah@ning.com', '123456789', '464-141-3262', '21037 American Ash Trail', 'https://robohash.org/rerumreiciendismagnam.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (19, 'Olly', 'obeetoni@biblegateway.com', '123456789', '377-877-8298', '37 Ridgeview Place', 'https://robohash.org/quaeratnihilquis.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (20, 'Vernon', 'vbaldacchij@msn.com', '123456789', '179-587-4991', '7 Farmco Court', 'https://robohash.org/reprehenderitrecusandaererum.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (21, 'Margaretta', 'maaronsonk@taobao.com', '123456789', '933-368-8608', '8450 Transport Parkway', 'https://robohash.org/enimsaepetotam.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (22, 'Ty', 'tdunckleel@state.tx.us', '123456789', '508-886-6242', '51630 Donald Alley', 'https://robohash.org/illoautmollitia.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (23, 'Drugi', 'ddedamm@comcast.net', '123456789', '159-369-9410', '51978 Comanche Avenue', 'https://robohash.org/rerumvitaemodi.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (24, 'Giordano', 'gmoiseyn@devhub.com', '123456789', '196-168-8102', '0830 Eggendart Junction', 'https://robohash.org/excepturiomnisquam.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (25, 'Una', 'upearmaino@usatoday.com', '123456789', '909-155-8516', '21 Merrick Place', 'https://robohash.org/sintconsequaturducimus.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (26, 'Carleton', 'cbthamp@independent.co.uk', '123456789', '267-380-8440', '66 Merrick Way', 'https://robohash.org/etipsaet.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (27, 'Clareta', 'cemmotq@xrea.com', '123456789', '343-704-3339', '80 Kennedy Place', 'https://robohash.org/veritatisquaeratveniam.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (28, 'Veronika', 'vprozillor@bluehost.com', '123456789', '985-520-1712', '2304 Anderson Alley', 'https://robohash.org/repudiandaeautducimus.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (29, 'Evan', 'etodarinis@dion.ne.jp', '123456789', '382-805-5548', '9355 Banding Circle', 'https://robohash.org/eiusutrerum.png?size=50x50&set=set1', 'user');
+insert into user (id, full-name, email, password, mobile, location, avater, rule) values (30, 'Leandra', 'lnezeyt@seesaa.net', '123456789', '261-332-9465', '1843 Farwell Trail', 'https://robohash.org/odioquiminima.png?size=50x50&set=set1', 'user');
