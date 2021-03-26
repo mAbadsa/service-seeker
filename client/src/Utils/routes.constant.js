abfced4d69e75a88e6d186e61205e63f1fd728e4
Osama-you <osama.w.younis@gmail.com>
2021-03-26 20:18:55 +0200

    fix PROVIDER_PROFILE_PAGE value Relates #58

--- client/src/Utils/routes.constant.js
+++ client/src/Utils/routes.constant.js
@@ -3,5 +3,5 @@ export const LOGIN_PAGE = '/login';
 export const REGISTER_PAGE = '/register';
 export const ORDERS_PAGE = '/orders';
 export const PROVIDER_DASHBOARD_PAGE = '/provider/dashboard';
-export const PROVIDER_PROFILE_PAGE = '/';
+export const PROVIDER_PROFILE_PAGE = '/provider/profile';
 export const ABOUT_US = '/about-us';
