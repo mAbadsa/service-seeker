9d454a7a0c20fb49535889d76b884f6d56e4c248
Osama-you <osama.w.younis@gmail.com>
2021-03-27 03:21:23 +0300

    create Auth Provider that set AuthContext value after fetch data from is-auth Relates #64

---
+++ client/src/Context/Authentication.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, createContext } from 'react';
+import Axios from 'axios';
+import PropTypes from 'prop-types';
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [isAuth, setIsAuth] = useState(false);
+  const [authLoading, setAuthLoading] = useState(true);
+  const [userData, setUserData] = useState({});
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    let unmounted = true;
+    let source = axios.CancelToken.source();
+    (async () => {
+      try {
+        setError(null);
+        const { data: userData } = await Axios('/api/v1/is-auth');
+        if (unmounted) {
+          setIsAuth(true);
+          setUserData(userData);
+          setAuthLoading(false);
+        }
+      } catch ({ response: resError }) {
+        setAuthLoading(false);
+        if (resError.status === 401) {
+          setIsAuth(false);
+          setUserData({});
+        } else {
+          setError(resError ? resError.data.message : 'internal server error.');
+        }
+      }
+    })();
+    return () => {
+      unmounted = false;
+      source.cancel('Cancelling in cleanup');
+    };
+  }, [isAuth]);
+
+  return (
+    <AuthContext.Provider
+      value={{ setIsAuth, isAuth, userData, authLoading, error }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthProvider;
