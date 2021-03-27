b8afa9ca2590c6b5520dd224732514df99dc82f1
Osama-you <osama.w.younis@gmail.com>
2021-03-27 03:30:34 +0300

    add new line

--- client/src/App/Routes/LoggedOutRoutes.js
+++ client/src/App/Routes/LoggedOutRoutes.js
@@ -13,6 +13,7 @@ const LoggedOutRoutes = ({ children, ...otherProps }) => {
   }
   return <Redirect to={HOME_PAGE} />;
 };
+
 LoggedOutRoutes.propTypes = {
   children: PropTypes.node.isRequired,
 };
