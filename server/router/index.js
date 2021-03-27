fd24c8e4135a25a71f090d32f3935e500f07cfae
Osama-you <osama.w.younis@gmail.com>
2021-03-27 13:38:33 +0300

    fix error typo Relates #62

--- server/router/index.js
+++ server/router/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
 const { userProtect } = require('../middleware/auth');
-const publicRouter = require('./publicRoutes');
+const publicRoutes = require('./publicRoutes');
 const user = require('./user');
 
-router.use(publicRouter);
+router.use(publicRoutes);
 router.use(userProtect);
 router.use(user);
 
