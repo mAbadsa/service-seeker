1c91ff6d5cb475c02d2ef9078786f63efbb93947
Osama-you <osama.w.younis@gmail.com>
2021-03-28 22:16:28 +0300

    add protectRoute for Router Relates #62

--- server/router/index.js
+++ server/router/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const { userProtect } = require('../middleware/auth');
+const { protectRoute } = require('../middleware/auth');
 const publicRoutes = require('./publicRoutes');
 const user = require('./user');
 
 router.use(publicRoutes);
-router.use(userProtect);
+router.use(protectRoute);
 router.use(user);
 
 module.exports = router;
