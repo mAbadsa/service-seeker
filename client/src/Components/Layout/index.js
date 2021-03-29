2c24da8b3f1c1ff781da5d305b81f343a9757843
zeinjendeya <z.jendeya@gmail.com>
2021-03-29 12:20:10 +0300

    changed style in header to dynamic

--- client/src/Components/Layout/index.js
+++ client/src/Components/Layout/index.js
@@ -1,29 +1,51 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
+//import { NotificationFilled } from '@ant-design/icons';
 
 const { Header, Footer, Content } = Layout;
+//const { SubMenu } = Menu;
+
+const headerStyle = {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: 'white',
+    boxShadow: '0px 4px 2px rgba(0, 0, 0, 0.16), 0px 8px 4px rgba(0, 0, 0, 0.08)'
+}
+
+const footerStyle = {
+    textAlign: 'center',
+    backgroundColor: '#FFEAA7',
+    fontSize: '14px'
+}
 
 const LayoutComponent = (props, isLogged, style) => {
-    const {children} = props;
+    const {children, userPic, userName} = props;
     const [current, setCurrent] = useState('home');
+    const history = useHistory();
 
     const handleMenu = (e)=>{
         setCurrent(e.key);
     }
     // expecting isLogged a boolean to specify if this person is logged or not
-
     return <Layout>
-        <Header>
-            <Menu onClick={handleMenu} selectedKeys={[current]} mode='horizontal'>
+        <Header style={headerStyle}>
+            <div style={{fontSize: '40px'}}>Hound</div>
+            <Menu style={{fontSize: '18px'}} onClick={handleMenu} selectedKeys={[current]} mode='horizontal'>
             <Menu.Item key='home'>Home</Menu.Item>
             {isLogged? <Menu.Item key='orders'>Orders</Menu.Item>: null}
             <Menu.Item key='aboutUs'>About Us</Menu.Item>
             </Menu>
+            {isLogged? <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+                <img src={userPic} alt={userName}/>
+                <p style={{fontSize: '13px'}}>{userName}</p>
+                </div>: <button onClick={history.push('/login')}>Sign In</button>}
             </Header>
         <Content style={style}>
             {children}
         </Content>
-        <Footer style={{ textAlign: 'center' }}>© 2021 Hound. All rights reserved.</Footer>
+        <Footer style={footerStyle}>© 2021 Hound. All rights reserved.</Footer>
     </Layout>
 }
 
