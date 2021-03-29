a3b60768c3be45b6d07388c22f4f71df35fafca4
Ahmed-aed <aeid767@gmail.com>
2021-03-29 12:20:32 +0300

    create proptypes and defaultprops related #28

--- client/src/Components/Input/index.js
+++ client/src/Components/Input/index.js
@@ -1,11 +1,27 @@
 import { Input } from 'antd';
-import './style.css'
-const CommonInput = (props) => {
-  return <Input className="input" placeholder={props.placeholder} 
-  onChange={props.onChange}/>;
+import PropTypes from 'prop-types';
+import './style.css';
+const CommonInput = ({ placeholder, handelChange, className, type, value }) => {
+  return (
+    <Input
+      className={className}
+      placeholder={placeholder}
+      type={type}
+      value={value}
+      onChange={handelChange}
+    />
+  );
+};
+CommonInput.prototype = {
+  placeholder: PropTypes.string.isRequired,
+  className: PropTypes.string,
+  handelChange: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
 };
 CommonInput.defaultProps = {
   placeholder: 'Enter your Full name',
-
+  className: 'input',
+  type: 'text',
 };
 export default CommonInput;
