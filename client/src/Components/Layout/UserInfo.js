81eb9f156333b79113a5fb7a968317ac373e1fad
zeinjendeya <z.jendeya@gmail.com>
2021-03-29 19:43:34 +0300

    logging out function

--- client/src/Components/Layout/UserInfo.js
+++ client/src/Components/Layout/UserInfo.js
@@ -1,23 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 import { LogoutOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 import Avatar from '../Avatar';
 import NotificationList from './notification';
 
-const UserInfo = (notifications, userPic, userName) => (
-  <div
-    style={{
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-    }}
-  >
-    <Avatar shape="circle" imgSrc={userPic} size="large" />
-    <p style={{ fontSize: '13px' }}>{userName}</p>
-    <NotificationList notifications={notifications} imgSrc={userPic} />
-    <LogoutOutlined />
-  </div>
-);
+const UserInfo = (notifications, userPic, userName) => {
+  const [clicked, setClicked] = useState(false);
+  const history = useHistory();
+
+  useEffect(async () => {
+    try {
+      const { CancelToken } = axios;
+      const source = CancelToken.source();
+      if (clicked) {
+        await axios.get('/api/v1/logout', {
+          cancelToken: source.token,
+        });
+        history.push('/');
+      }
+      return () => source.cancel('Operation canceled by the user.');
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err);
+      return err;
+    }
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+      }}
+    >
+      <Avatar shape="circle" imgSrc={userPic} size="large" />
+      <p style={{ fontSize: '13px' }}>{userName}</p>
+      <NotificationList notifications={notifications} imgSrc={userPic} />
+      <LogoutOutlined onClick={setClicked(true)} />
+    </div>
+  );
+};
 
 UserInfo.propType = {
   notifications: PropTypes.arrayOf(
