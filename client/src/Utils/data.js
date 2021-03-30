258c1053e07f2a0320e95baed02401d2711d9c4e
zeinjendeya <z.jendeya@gmail.com>
2021-03-30 18:16:55 +0300

    added data for services and locations

---
+++ client/src/Utils/data.js
@@ -0,0 +1,28 @@
+const serviceTypes = [
+  'plumber',
+  'Electrician',
+  'carpenter',
+  'mechanic',
+  'builder',
+];
+
+const locations = [
+  'gaza',
+  'haifa',
+  'yafa',
+  'jerusalem',
+  'jericho',
+  'hebron',
+  'khan younis',
+  'safad',
+  'rafah',
+  'jabalia',
+  'nusirat',
+  'maghazi',
+  'dir al-balah',
+];
+
+export default {
+  serviceTypes,
+  locations,
+};
