54519f41410dc7bda75d5424e5845477fc059d1a
Osama-you <osama.w.younis@gmail.com>
2021-03-31 21:27:50 +0300

    remove status

--- server/controller/provider/providersList.js
+++ server/controller/provider/providersList.js
@@ -3,7 +3,7 @@ const { getAllProviders } = require('../../database/queries/provider');
 const providersList = async (req, res, next) => {
   try {
     const { rows } = await getAllProviders();
-    res.status(200).json({
+    res.json({
       statusCode: 200,
       data: rows,
     });
