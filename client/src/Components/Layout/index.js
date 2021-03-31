f99416bffc56530a467e1cb264d5e4222bf691e9
zeinjendeya <z.jendeya@gmail.com>
2021-03-31 21:31:51 +0300

    seperated folders

--- client/src/Components/Layout/index.js
+++ client/src/Components/Layout/index.js
@@ -1,5 +1,21 @@
-import Content from './content';
-import NavBar from './navBar';
-import Footer from './footer';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Layout } from 'antd';
+import Footer from '../Footer';
+import Header from '../NavBar';
 
-export { Content, NavBar, Footer };
+const { Content } = Layout;
+
+const LayoutComponent = ({ children }) => {
+  <Layout>
+    <Header></Header>
+    <Content>{children}</Content>
+    <Footer className="footer">© 2021 Dribbble. All rights reserved.</Footer>;
+  </Layout>;
+};
+
+LayoutComponent.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default LayoutComponent;
