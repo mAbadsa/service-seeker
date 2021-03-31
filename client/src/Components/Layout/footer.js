8fd5e7b929abc7daccc51e2544e9bb63ac1b9b69
zeinjendeya <z.jendeya@gmail.com>
2021-03-31 12:04:42 +0300

    generic footer element

---
+++ client/src/Components/Layout/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Layout } from 'antd';
+
+const { Footer } = Layout;
+
+const Foot = ({ children, className }) => {
+  <Footer className={className}>{children}</Footer>;
+};
+
+Foot.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string.isRequired,
+};
+
+export default Foot;
