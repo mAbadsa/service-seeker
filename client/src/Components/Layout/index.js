75d3c00efeb8b0a011c0ddb4546560de8aaf6d5e
zeinjendeya <z.jendeya@gmail.com>
2021-03-31 13:34:15 +0300

    collected items from layout folder

--- client/src/Components/Layout/index.js
+++ client/src/Components/Layout/index.js
@@ -1,38 +1,9 @@
-import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
-import { Layout } from 'antd';
-import HeaderLogged from './headerLog';
-import HeaderPublic from './headerPublic';
-import AuthProvider from '../../Context/Authentication';
+import Content from './content';
+import NavBar from './navBar';
+import Footer from './footer';
 
-import './style.css';
-
-const { Header, Footer, Content } = Layout;
-
-const LayoutComponent = ({ children, userPic, userName }) => {
-  const { isAuth } = useContext(AuthProvider);
-  return (
-    <Layout>
-      <Header className="header">
-        <div className="logo">Hound</div>
-        {isAuth ? (
-          <>
-            <HeaderLogged userPic={userPic} userName={userName} />
-          </>
-        ) : (
-          <HeaderPublic />
-        )}
-      </Header>
-      <Content>{children}</Content>
-      <Footer className="footer">© 2021 Hound. All rights reserved.</Footer>
-    </Layout>
-  );
+export default {
+  Content,
+  NavBar,
+  Footer,
 };
-
-LayoutComponent.propTypes = {
-  children: PropTypes.node.isRequired,
-  userPic: PropTypes.string,
-  userName: PropTypes.string.isRequired,
-};
-
-export default LayoutComponent;
