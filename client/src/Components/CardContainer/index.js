8ecdb973919f3a1d139756ff38510defbfc199b0
Osama-you <osama.w.younis@gmail.com>
2021-04-01 10:59:30 +0300

    add Pagination Relates #26

--- client/src/Components/CardContainer/index.js
+++ client/src/Components/CardContainer/index.js
@@ -1,36 +1,43 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
-import { Typography } from 'antd';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Empty, Pagination, Typography } from 'antd';
 
 import './style.css';
 
 const { Title } = Typography;
-const cardContainer = ({ title, data, ...rest }) => (
-  <div {...rest}>
+
+const cardContainer = ({ title, data, ...rest }) => {
+  const numEachPage = 10;
+  const [limit, setLimit] = useState([0, numEachPage]);
+  <div className="container" {...rest}>
     <Title id="container-title" level={2}>
       {title}
     </Title>
     <div className="card-container">
-      {data &&
-        data.map((item) => (
-          <div
-            key={item.id}
-            style={{
-              height: '190px',
-              width: '974px',
-              backgroundColor: 'red',
-              borderRadius: '20px',
-            }}
-          >
-            <div></div>
-          </div>
-        ))}
+      {data.length !== 0 ? (
+        data.slice(limit[0], limit[1]).map((item) => <>{item}</>)
+      ) : (
+        <Empty description="لا يوجد رياض أطفال" />
+      )}
     </div>
-  </div>
-);
+    <Pagination
+      total={data.length}
+      onChange={(value) => {
+        setLimit([(value - 1) * numEachPage, value * numEachPage]);
+      }}
+      showSizeChanger={false}
+    />
+  </div>;
+};
 
 cardContainer.propTypes = {
-  // data:PropTypes.o
+  title: PropTypes.string,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      // there's more
+    })
+  ),
 };
 // cardContainer.defaultProps = {};
 
