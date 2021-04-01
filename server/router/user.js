b0df46d3f3a7e4f46d71bf2e1113e3693727204e
Muhammad Alabadsa <moh22389@gmail.com>
2021-04-01 18:56:20 +0300

    Add validation for the order request Relates #47

--- server/router/user.js
+++ server/router/user.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 
 const { isAuthController, userOrderReguest } = require('../controller/user');
+const { orderRequestValidation } = require('../middleware/validation');
 
 router.get('/is-auth', isAuthController);
-router.post('/user/order-request', userOrderReguest);
+router.post('/user/order-request', orderRequestValidation, userOrderReguest);
 
 module.exports = router;
