1505373321f4256998d31a8063d80828c97462de
zeinjendeya <z.jendeya@gmail.com>
2021-04-01 14:12:56 +0300

    final layout page structure and logic relates #73

--- client/src/Pages/Layout/index.js
+++ client/src/Pages/Layout/index.js
@@ -1,29 +1,13 @@
 import React, { useContext } from 'react';
 import { element } from 'prop-types';
-import { Spin, Layout } from 'antd';
-import { Content, NavBar, Footer } from '../../Components/Layout';
+import { Spin } from 'antd';
+import Layout from '../../Components/Layout';
 import AuthProvider from '../../Context/Authentication';
-
-// if authLoading is still true keep the loading
-// if auth check role
-// if role is customer show customer navbar
-// if role is craftsman show craftsman navbar
-// content will always be children from props
 
 const LayoutPage = ({ children }) => {
   const { authLoading } = useContext(AuthProvider);
   return (
-    <>
-      {authLoading ? (
-        <Spin size="large" />
-      ) : (
-        <Layout>
-          <NavBar></NavBar>
-          <Content>{children}</Content>
-          <Footer></Footer>
-        </Layout>
-      )}
-    </>
+    <>{authLoading ? <Spin size="large" /> : <Layout>{children}</Layout>}</>
   );
 };
 
