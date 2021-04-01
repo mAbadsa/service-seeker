af874ee80ee683c10bba734f774756d1f4f564b3
Osama-you <osama.w.younis@gmail.com>
2021-04-01 18:15:11 +0300

    update the style

--- client/src/Components/Card/index.js
+++ client/src/Components/Card/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card, Row, Col, Image, Typography, Rate } from 'antd';
 
-import { Card, Row, Col, Image, Typography, Rate, Avatar } from 'antd';
+import Button from '../Button';
+import Avatar from '../Avatar';
+
 import './style.css';
-
-import CommonButton from '../Button';
 
 const { Title, Text, Paragraph } = Typography;
 
@@ -20,73 +21,70 @@ const CommonCard = ({
 }) => (
   <Card className="cardStyle">
     <Row gutter={[16, 16]} type="flex" justify="start">
-      <Col sm={24} md={6} lg={6}>
-        <Image src={ImageSrc} className="imageStyle" />
+      <Col sm={24} lg={6} className="imageContener">
+        <Image src={ImageSrc} className="imageStyle" preview={false} />
       </Col>
-      <Col sm={24} md={14} lg={14} className="cardDescription">
-        <Title level={4}>{TitleJob}</Title>
-        <div>
-          <Rate value={rate} />
+      <Col sm={24} lg={14} className="cardDescription">
+        <Title id="cardTitle" level={4}>
+          {TitleJob}
+        </Title>
+        <div className="cardDescription__top">
+          <div>
+            <Text type="secondary" className="cardRating__text">
+              {rate}
+            </Text>
+            <Rate value={rate} className="cardRating__stars" disabled />
+          </div>
           <Text type="secondary" className="cardLocation">
             location:
             <Text>{city}</Text>
           </Text>
         </div>
         <div>
-          <Text type="secondary" className="cardLocation">
+          <Text type="secondary" className="cardPrice">
             Price:
             <Text>{priceByHour}$</Text>
           </Text>
         </div>
         <Paragraph
           ellipsis={{
-            rows: 4,
+            rows: 3,
           }}
+          id="cardDes"
         >
           {descriptions}
         </Paragraph>
       </Col>
-      <Col sm={24} md={4} lg={4}>
+      <Col sm={24} lg={4} className="cardHireContener">
         <div className="cardHireMe">
           <div>
-            <Avatar size={100} src={avatarImage} />
+            <Avatar size={75} className="avatarCard" srcImg={avatarImage} />
             <div>
               <Text>{Name}</Text>
             </div>
           </div>
-          <CommonButton
+          <Button
             handelClick={onclick}
             type="primary"
-            className="initial-style primaryButton"
+            className="initial-style fourthButton hireBtn"
           >
             Hire me
-          </CommonButton>
+          </Button>
         </div>
       </Col>
     </Row>
   </Card>
 );
+
 CommonCard.propTypes = {
-  priceByHour: PropTypes.float,
-  ImageSrc: PropTypes.string,
-  TitleJob: PropTypes.string,
-  city: PropTypes.string,
-  rate: PropTypes.Integer,
-  descriptions: PropTypes.string,
-  Name: PropTypes.string,
-  avatarImage: PropTypes.string,
-};
-CommonCard.defaultProps = {
-  priceByHour: 17.5,
-  ImageSrc:
-    'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  avatarImage:
-    'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-  TitleJob: 'developer',
-  city: 'Gaza',
-  rate: 3.5,
-  descriptions: 'welcome im my Job card ',
-  Name: 'Reinald',
+  priceByHour: PropTypes.number.isRequired,
+  ImageSrc: PropTypes.string.isRequired,
+  TitleJob: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  rate: PropTypes.number.isRequired,
+  descriptions: PropTypes.string.isRequired,
+  Name: PropTypes.string.isRequired,
+  avatarImage: PropTypes.string.isRequired,
 };
 
 export default CommonCard;
