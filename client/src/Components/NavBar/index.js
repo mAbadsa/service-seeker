b5f86c1a9fe48ff66e9b3d36ce3349fc48f87bc6
zeinjendeya <z.jendeya@gmail.com>
2021-04-01 14:01:45 +0300

    final navbar structure and logic

--- client/src/Components/NavBar/index.js
+++ client/src/Components/NavBar/index.js
@@ -1,26 +1,69 @@
 import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
+import Axios from 'axios';
 import { Layout, Typography } from 'antd';
 import AuthProvider from '../../Context/Authentication';
 import Public from './Public';
-import User from './User';
+import UserCustomer from './User';
+import ProviderMenu from './Provider';
 
 const { Header } = Layout;
 const { Title } = Typography;
 
-const NavBar = ({ className }) => {
-  const { setIsAuth, isAuth } = useContext(AuthProvider);
+const NavBar = ({ className, notifications }) => {
+  const {
+    setIsAuth,
+    isAuth,
+    userData: { role },
+  } = useContext(AuthProvider);
   const [isLoading, setLoading] = useState(false);
+  const [errMsg, setErrMsg] = useState('');
+
+  const handleClick = async () => {
+    try {
+      setLoading(true);
+      await Axios('api/v1/logout');
+      setIsAuth(false);
+    } catch (err) {
+      setErrMsg('set interval error');
+    }
+  };
+
   <Header className={className}>
     <Title level={4}>Hound</Title>
-    <Public />
-    <User />
+    {!isAuth ? (
+      <Public />
+    ) : (
+      <>
+        {role === 'user' ? (
+          <UserCustomer
+            handleClick={handleClick}
+            errMsg={errMsg}
+            isLoading={isLoading}
+            notifications={notifications}
+          />
+        ) : (
+          <ProviderMenu
+            handleClick={handleClick}
+            errMsg={errMsg}
+            isLoading={isLoading}
+          />
+        )}
+      </>
+    )}
   </Header>;
 };
 
 NavBar.propTypes = {
-  children: PropTypes.node.isRequired,
   className: PropTypes.string,
+  notifications: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      user_id: PropTypes.number,
+      decription: PropTypes.string,
+      created_at: PropTypes.string,
+    })
+  ),
 };
 
 export default NavBar;
