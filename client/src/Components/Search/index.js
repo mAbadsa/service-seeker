acb494197c300133948612722da40fe6679dd773
zeinjendeya <z.jendeya@gmail.com>
2021-04-04 11:16:10 +0300

    removed handlers from search component

--- client/src/Components/Search/index.js
+++ client/src/Components/Search/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { SearchOutlined } from '@ant-design/icons';
 import Button from '../Button';
@@ -8,46 +8,35 @@ import './style.css';
 
 import { locations, serviceTypes } from '../../Utils/data';
 
-const Filter = ({ handleSearch }) => {
-  const [service, setService] = useState('null');
-  const [location, setLocation] = useState('null');
-
-  handleSearch(service, location);
-
-  const handleService = (value) => {
-    setService(value);
-  };
-  const handleLocation = (value) => {
-    setLocation(value);
-  };
-  return (
-    <div className="filter-container">
-      <Select
-        className="filter-select"
-        textDefault={serviceTypes[0]}
-        handleChange={handleService}
-        options={serviceTypes}
-      />
-      <p className="split-text">in</p>
-      <Select
-        className="filter-select"
-        textDefault={locations[0]}
-        handleChange={handleLocation}
-        options={locations}
-      />
-      <Button
-        type="primaryButton"
-        handelClick={handleSearch}
-        icon={<SearchOutlined />}
-      >
-        Search
-      </Button>
-    </div>
-  );
-};
+const Filter = ({ handleSearch, handleService, handleLocation }) => (
+  <div className="filter-container">
+    <Select
+      className="filter-select"
+      textDefault={serviceTypes[0]}
+      handleChange={handleService}
+      options={serviceTypes}
+    />
+    <p className="split-text">in</p>
+    <Select
+      className="filter-select"
+      textDefault={locations[0]}
+      handleChange={handleLocation}
+      options={locations}
+    />
+    <Button
+      type="primaryButton"
+      handelClick={handleSearch}
+      icon={<SearchOutlined />}
+    >
+      Search
+    </Button>
+  </div>
+);
 
 Filter.propTypes = {
   handleSearch: PropTypes.func.isRequired,
+  handleService: PropTypes.func.isRequired,
+  handleLocation: PropTypes.func.isRequired,
 };
 
 export default Filter;
