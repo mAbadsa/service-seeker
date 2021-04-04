8b0f82639de65231c963c5076f55ee8ade83cdfb
Muhammad Alabadsa <moh22389@gmail.com>
2021-04-04 20:24:46 +0300

    Add user image and details input Relates #101

--- client/src/Pages/Orders/index.js
+++ client/src/Pages/Orders/index.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Row, Col, Image, Typography, Rate, Space } from 'antd';
+import { Row, Col, Typography, Rate, Input } from 'antd';
+
+import Button from '../../Components/Button';
 
 import './style.css';
 
-const { Title, Paragraph } = Typography;
+const { Title, Paragraph, Text } = Typography;
+const { TextArea } = Input;
 
 function Orders({
+  username,
   coverImage,
   userAvatar,
   professionTitle,
@@ -16,13 +20,15 @@ function Orders({
   price,
 }) {
   const [rateValue, setRateValue] = useState(2.5);
+  const [isLoading, setIsLoading] = useState(true);
 
   const handleChange = (value) => {
     setRateValue(value);
+    setIsLoading(true);
   };
 
   return (
-    <Row className="Orders">
+    <Row className="Orders" gutter={[16, 16]}>
       <Col
         className="bg-image"
         style={{
@@ -30,64 +36,93 @@ function Orders({
         }}
         span="24"
       ></Col>
-      <Col span="24">
+      <Col span={24}>
         <Row className="Orders-card">
-          <Col xs={24} sm={24} md={5}>
-            <Image width="180" src={userAvatar} />
+          <Col xs={24} sm={24} md={8}>
+            <div
+              className="image-container"
+              style={{
+                backgroundImage: `url(${userAvatar})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+              }}
+            >
+              <Text value={5}>{username}</Text>
+            </div>
           </Col>
-          <Space>
-            <Col span="17" offset={1}>
-              <Row align="middle">
-                <Title level={5}>{professionTitle}</Title>
-              </Row>
-              <Row type="flex" justify="start">
-                <Col xs={24} sm={24} md={11} justify="start">
-                  <span className="label-text label-text__rating label-text__emp">
-                    {rateValue}
-                  </span>
-                  <Rate
-                    className="rating"
-                    allowHalf
-                    defaultValue={2.5}
-                    value={rateValue}
-                    onChange={handleChange}
-                  />
-                  <span className="label-text">{`(${ratings} ${
-                    ratings === 1 ? 'rating' : 'ratings'
-                  })`}</span>
-                </Col>
-                <Col span={7} xs={24} sm={24} md={6}>
-                  <span className="label-text">
-                    Location:{' '}
-                    <span className="label-text__emp">{location}</span>
-                  </span>
-                </Col>
-                <Col span={7} xs={24} sm={24} md={7}>
-                  <span className="label-text">
-                    Mobile: <span className="label-text__emp">{mobile}</span>
-                  </span>
-                </Col>
-              </Row>
-              <Row>
-                <Col sm="24">
-                  <span className="label-text">
-                    Price: $ <span className="label-text__emp">{price}</span>/H
-                  </span>
-                </Col>
-              </Row>
-              <Row>
-                <Col sm="24">
-                  <Paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nulla varius elit erat odio dictum felis dolor adipiscing
-                    varius. Nisl bibendum orci in eleifend proin. Leo at lacus,
-                    iaculis aliquet. Felis, turpis dui, rhoncus massa id nisl
-                    rutrum sapien. Eu le...
-                  </Paragraph>
-                </Col>
-              </Row>
-            </Col>
-          </Space>
+          <Col xm={24} md={16} className="Orders-card__details">
+            <Row align="middle">
+              <Title level={5}>{professionTitle}</Title>
+            </Row>
+            <Row>
+              <Col xs={24} sm={24} md={12}>
+                <span className="label-text label-text__rating label-text__emp">
+                  {rateValue}
+                </span>
+                <Rate
+                  className="rating"
+                  allowHalf
+                  defaultValue={2.5}
+                  value={rateValue}
+                  onChange={handleChange}
+                />
+                <span className="label-text">{`(${ratings} ${
+                  ratings === 1 ? 'rating' : 'ratings'
+                })`}</span>
+              </Col>
+              <Col xs={24} sm={24} md={5}>
+                <span className="label-text">
+                  Location: <span className="label-text__emp">{location}</span>
+                </span>
+              </Col>
+              <Col xs={24} sm={24} md={7}>
+                <span className="label-text">
+                  Mobile: <span className="label-text__emp">{mobile}</span>
+                </span>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm="24">
+                <span className="label-text">
+                  Price: $ <span className="label-text__emp">{price}</span>/H
+                </span>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm="24">
+                <Paragraph
+                  ellipsis={{
+                    rows: 3,
+                  }}
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+                  varius elit erat odio dictum felis dolor adipiscing varius.
+                  Nisl bibendum orci in eleifend proin. Leo at lacus, iaculis
+                  aliquet. Felis, turpis dui, rhoncus massa id nisl rutrum
+                  sapien. Eu le...
+                </Paragraph>
+              </Col>
+            </Row>
+          </Col>
+          <Col span={24} className="Orders-detail-input">
+            <TextArea
+              placeholder="Controlled autosize"
+              autoSize={{
+                minRows: 3,
+                maxRows: 5,
+              }}
+            />
+          </Col>
+          <Col span={24}>
+            <Button
+              className="hireme-btn"
+              type="primary"
+              htmlType="submit"
+              loading={isLoading}
+            >
+              Hire me
+            </Button>
+          </Col>
         </Row>
       </Col>
     </Row>
@@ -95,6 +130,7 @@ function Orders({
 }
 
 Orders.defaultProps = {
+  username: 'Username',
   coverImage: '/static/order-bg.png',
   userAvatar: '/static/avatar.png',
   professionTitle: 'Electrician',
@@ -105,6 +141,7 @@ Orders.defaultProps = {
 };
 
 Orders.propTypes = {
+  username: PropTypes.string.isRequired,
   coverImage: PropTypes.string.isRequired,
   userAvatar: PropTypes.string.isRequired,
   professionTitle: PropTypes.string.isRequired,
