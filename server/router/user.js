0a14de174c9941f0a39699b1ec2da4ef773a4701
zeinjendeya <z.jendeya@gmail.com>
2021-04-05 15:50:46 +0300

    added route controller to protected routes

--- server/router/user.js
+++ server/router/user.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const { isAuthController } = require('../controller/user');
+const { deleteOrderController } = require('../controller');
 
 router.get('/is-auth', isAuthController);
+router.delete('/user/order-requests/:orderReqId', deleteOrderController);
 
 module.exports = router;
