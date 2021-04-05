943d67ecac3d23fbee8d34bd742bde00b0921eab
Osama-you <osama.w.younis@gmail.com>
2021-04-05 20:48:42 +0300

    update UserInfo structure Relates #73

--- client/src/Components/UserInfo/index.js
+++ client/src/Components/UserInfo/index.js
@@ -1,17 +1,20 @@
 import React, { useContext, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-
-import { Spin, Alert } from 'antd';
-import { LogoutOutlined, BellOutlined } from '@ant-design/icons';
 import Axios from 'axios';
+import { Spin, message, Typography } from 'antd';
+import { LogoutOutlined, BellOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
+
 import Avatar from '../Avatar';
 import { AuthContext } from '../../Context/Authentication';
 import { HOME_PAGE } from '../../Utils/routes.constant';
 
+import './style.css';
+
+const { Text } = Typography;
+
 const UserInfo = () => {
   const [isLoading, setLoading] = useState(false);
-  const [errMsg, setErrMsg] = useState('');
   const { userData, setRefresh, refresh, setAuthLoading } = useContext(
     AuthContext
   );
@@ -20,33 +23,41 @@ const UserInfo = () => {
   const handleClick = async () => {
     try {
       setLoading(true);
-      setErrMsg('');
       await Axios('/api/v1/logout');
       setRefresh(!refresh);
       setLoading(false);
       setAuthLoading(true);
       history.push(HOME_PAGE);
     } catch (err) {
-      setErrMsg('interval server error');
+      message.error('Something went wrong!');
       setLoading(false);
     }
   };
+
   return (
-    <>
-      <div className="logged-user-info">
-        {userData ? (
-          <>
-            <Avatar shape="circle" srcImg={userData.avatar} size="large" />
-            <p className="user-name">{userData.username}</p>
-            <BellOutlined />
-            {isLoading ? <Spin /> : <LogoutOutlined onClick={handleClick} />}
-          </>
-        ) : (
-          <spin />
-        )}
-      </div>
-      {errMsg && <Alert message={errMsg} type="error" />}
-    </>
+    <div className="logged-user-info">
+      {userData ? (
+        <>
+          <Avatar
+            shape="circle"
+            srcImg={userData.avatar}
+            size={44}
+            style={{
+              minWidth: '43px',
+            }}
+          />
+          <Text className="user-name">{userData.username}</Text>
+          <BellOutlined className="UserInfo-icon" />
+          {isLoading ? (
+            <Spin className="UserInfo-icon" />
+          ) : (
+            <LogoutOutlined className="UserInfo-icon" onClick={handleClick} />
+          )}
+        </>
+      ) : (
+        <spin />
+      )}
+    </div>
   );
 };
 
