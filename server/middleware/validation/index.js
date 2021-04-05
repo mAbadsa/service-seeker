1721c99e4928bec305c4fda80ad3da6d9df041c2
zeinjendeya <z.jendeya@gmail.com>
2021-04-05 16:03:44 +0300

    created validation time arrive in request function

--- server/middleware/validation/index.js
+++ server/middleware/validation/index.js
@@ -1,7 +1,9 @@
 const signupValidation = require('./signup');
 const loginValidation = require('./login');
+const orderAcceptValidation = require('./orderProviderAccept');
 
 module.exports = {
   signupValidation,
   loginValidation,
+  orderAcceptValidation,
 };
---
+++ server/middleware/validation/orderProviderAccept.js
@@ -0,0 +1,19 @@
+const { object, date } = require('yup');
+
+const { boomify } = require('../../utils');
+
+const orderAcceptValidation = async (req, res, next) => {
+  try {
+    const schema = object().shape({
+      arrive_time: date(),
+    });
+    await schema.validate(req.body, {
+      abortEarly: false,
+    });
+    next();
+  } catch (err) {
+    next(boomify(400, err.errors[0]));
+  }
+};
+
+module.exports = orderAcceptValidation;
