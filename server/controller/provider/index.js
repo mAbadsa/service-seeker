bf602ab98b21278efc173cf5645e7377dd11962d
zeinjendeya <z.jendeya@gmail.com>
2021-04-05 15:35:47 +0300

    added controller to indexes

--- server/controller/provider/index.js
+++ server/controller/provider/index.js
@@ -1,5 +1,7 @@
 const providersListController = require('./providersList');
+const deleteOrderController = require('./deleteOrder');
 
 module.exports = {
   providersListController,
+  deleteOrderController,
 };
