48bc80eedfadc66724e6937e95dcd3c15e41716c
zeinjendeya <z.jendeya@gmail.com>
2021-04-05 15:21:57 +0300

    deleted check row count from controller

--- server/controller/provider/deleteOrder.js
+++ server/controller/provider/deleteOrder.js
@@ -1,14 +1,13 @@
 const { deleteOrder } = require('../../database/queries');
-const { boomify } = require('../../utils');
 
 const deleteOrderController = async (req, res, next) => {
+  const { id: userId } = req.user;
   const { orderReqId } = req.params;
   try {
-    const { rowCount } = await deleteOrder(orderReqId);
-    if (rowCount === 0) {
-      throw boomify(404, 'order-request not found');
-    }
-    res.status(200).json({ status: 200, message: 'deleted successfully' });
+    await deleteOrder(orderReqId, userId);
+    res
+      .status(200)
+      .json({ status: 200, message: 'order deleted successfully' });
   } catch (err) {
     next(err);
   }
