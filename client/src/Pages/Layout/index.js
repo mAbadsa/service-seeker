6bf60181df07357eac5e2ee239e1e57bad5faadf
Osama-you <osama.w.younis@gmail.com>
2021-04-06 00:10:06 +0300

    update the LayoutPage

--- client/src/Pages/Layout/index.js
+++ client/src/Pages/Layout/index.js
@@ -1,26 +1,38 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import { Layout, Spin } from 'antd';
 import Footer from '../../Components/Footer';
 import Header from '../../Components/NavBar';
 
 import { AuthContext } from '../../Context/Authentication';
+import {
+  LOGIN_PAGE,
+  REGISTER_PAGE,
+  PROVIDER_DASHBOARD_PAGE,
+} from '../../Utils/routes.constant';
 
 import './style.css';
 
 const { Content } = Layout;
 
+const withoutLayout = [LOGIN_PAGE, REGISTER_PAGE, PROVIDER_DASHBOARD_PAGE];
+
 const LayoutPage = ({ children }) => {
   const { authLoading } = useContext(AuthContext);
+  const { pathname } = useLocation();
+
+  const isWithLayout = !withoutLayout.includes(pathname);
+
   return (
     <>
       {authLoading ? (
         <Spin size="large" className="loading" />
       ) : (
         <>
-          <Header />
+          {isWithLayout && <Header />}
           <Content className="page">{children}</Content>
-          <Footer />
+          {isWithLayout && <Footer />}
         </>
       )}
     </>
