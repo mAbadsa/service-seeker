8e84a8f6e2fc2edeea6cf4c87222f48d33f7fdbd
zeinjendeya <z.jendeya@gmail.com>
2021-04-05 17:01:49 +0300

    added route controller to router user protected routes file

--- server/router/user.js
+++ server/router/user.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
 const { isAuthController } = require('../controller/user');
+const { orderAcceptValidation } = require('../middleware/validation');
+const { acceptOrderController } = require('../controller');
 
 router.get('/is-auth', isAuthController);
+router.post('/provider/orders', orderAcceptValidation, acceptOrderController);
 
 module.exports = router;
