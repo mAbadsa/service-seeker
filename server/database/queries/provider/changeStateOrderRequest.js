1a43aa8b678422b45ea4e3fc053827b362168c9e
zeinjendeya <z.jendeya@gmail.com>
2021-04-05 16:27:40 +0300

    made query to set the order state to accepted

---
+++ server/database/queries/provider/changeStateOrderRequest.js
@@ -0,0 +1,12 @@
+const connection = require('../../config/connection');
+
+const updateStateOrderRequest = (orderID) => {
+  const sql = {
+    text: "UPDATE ordes_request SET state='accepted WHERE id=$1'",
+    values: [orderID],
+  };
+
+  return connection.query(sql);
+};
+
+module.exports = updateStateOrderRequest;
