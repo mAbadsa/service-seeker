542890c8aa36b81d57729a0a6ddc670b0cb7a427
Muhammad Alabadsa <moh22389@gmail.com>
2021-04-06 17:10:32 +0300

    Solve conflict Relates #47

--- server/database/queries/index.js
+++ server/database/queries/index.js
@@ -1,21 +1,16 @@
-<<<<<<< HEAD
-const { createNewUser, checkUserByEmail, addOrderRequest } = require('./user');
-=======
 const {
   createNewUser,
   checkUserByEmail,
+  addOrderRequest,
   getUserOrdersReqQuery,
 } = require('./user');
->>>>>>> 2d3dc7da49a3f9b7cd407ba687085eb54ad00109
+
 const { createNewProvider } = require('./provider');
 
 module.exports = {
   createNewUser,
   checkUserByEmail,
   createNewProvider,
-<<<<<<< HEAD
   addOrderRequest,
-=======
   getUserOrdersReqQuery,
->>>>>>> 2d3dc7da49a3f9b7cd407ba687085eb54ad00109
 };
--- server/database/queries/user/index.js
+++ server/database/queries/user/index.js
@@ -1,19 +1,13 @@
 const checkUserByEmail = require('./checkUserByEmail');
 const createNewUser = require('./createNewUser');
 const getUserData = require('./getUserData');
-<<<<<<< HEAD
 const addOrderRequest = require('./addOrderRequest');
-=======
 const getUserOrdersReqQuery = require('./getUserOrdersReq');
->>>>>>> 2d3dc7da49a3f9b7cd407ba687085eb54ad00109
 
 module.exports = {
   checkUserByEmail,
   createNewUser,
   getUserData,
-<<<<<<< HEAD
   addOrderRequest,
-=======
   getUserOrdersReqQuery,
->>>>>>> 2d3dc7da49a3f9b7cd407ba687085eb54ad00109
 };
