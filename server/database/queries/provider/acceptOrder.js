c4147b1a822a4cae9de4960a622880526ce47ee9
zeinjendeya <z.jendeya@gmail.com>
2021-04-07 14:02:05 +0300

    fixed query

--- server/database/queries/provider/acceptOrder.js
+++ server/database/queries/provider/acceptOrder.js
@@ -3,7 +3,7 @@ const connection = require('../../config/connection');
 const acceptOrder = (arriveTime, ordersRequestId, state) => {
   const sql = {
     text:
-      'INSERT INTO orders (ordes_request_id, state, arrive_time) VALUES ($1, $2, $3)',
+      'INSERT INTO orders (orders_request_id, state, arrive_time) VALUES ($1, $2, $3)',
     values: [ordersRequestId, state, arriveTime],
   };
 
--- server/database/queries/provider/changeStateOrderRequest.js
+++ server/database/queries/provider/changeStateOrderRequest.js
@@ -3,7 +3,7 @@ const connection = require('../../config/connection');
 const updateStateOrderRequest = (orderID, providerID) => {
   const sql = {
     text:
-      "UPDATE ordes_request SET state='accepted' WHERE id=$1 AND provider_id=$2'",
+      "UPDATE orders_request SET state='accepted' WHERE id=$1 AND provider_id=$2",
     values: [orderID, providerID],
   };
 
