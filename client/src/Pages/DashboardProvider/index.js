ceae6e66205aba033f9e87b01b339ffc5581a59f
Ahmed-aed <aeid767@gmail.com>
2021-04-07 16:17:25 +0300

    responsive style for sider #111

--- client/src/Pages/DashboardProvider/index.js
+++ client/src/Pages/DashboardProvider/index.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { Layout, Menu, Typography } from 'antd';
+import { Layout, Menu, Typography, Grid, Drawer } from 'antd';
 import {
   AppstoreOutlined,
   BellOutlined,
   UserOutlined,
   BellFilled,
+  MenuOutlined,
+  CloseOutlined,
 } from '@ant-design/icons';
-
 import Avatar from '../../Components/Avatar';
 import './style.css';
 
 const { Sider, Content } = Layout;
 const { Text } = Typography;
+const { useBreakpoint } = Grid;
+const DashboardProvider = () => {
+  const { md } = useBreakpoint();
+  const [visible, setVisible] = useState(false);
 
-const DashboardProvider = () => (
-  <Layout className="layout">
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+  const mySider = (
     <Sider>
       <div className="logo">
         <Avatar size={100} />
@@ -34,17 +45,36 @@ const DashboardProvider = () => (
         </Menu.Item>
       </Menu>
     </Sider>
+  );
+  return (
+    <Layout className="layout">
+      {md ? mySider : null}
 
-    <Layout className="site-layout">
-      <Content className="site-layout-background">
-        <div className="layoutHeder">
-          <Text>Profile</Text>
-          <BellFilled />
-        </div>
-      </Content>
+      <Layout className="site-layout">
+        <Content className="site-layout-background">
+          <div className="layoutHeder">
+            {!md ? (
+              <>
+                <MenuOutlined onClick={showDrawer} />
+                <Drawer
+                  title={<CloseOutlined onClick={onClose} />}
+                  placement="left"
+                  closable={false}
+                  onClose={onClose}
+                  visible={visible}
+                >
+                  {mySider}
+                </Drawer>
+              </>
+            ) : null}
+            <Text>Profile</Text>
+            <BellFilled />
+          </div>
+        </Content>
+      </Layout>
     </Layout>
-  </Layout>
-);
+  );
+};
 DashboardProvider.propTypes = {
   children: PropTypes.node,
 };
