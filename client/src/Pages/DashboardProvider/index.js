10b760976e8a3e80053a3ab96ba0ef415aacf73e
Ahmed-aed <aeid767@gmail.com>
2021-04-08 01:20:50 +0300

    responsive sider ,logout button and avalibilty switch relates #111

--- client/src/Pages/DashboardProvider/index.js
+++ client/src/Pages/DashboardProvider/index.js
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import { Layout, Menu, Typography, Grid, Drawer } from 'antd';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Axios from 'axios';
+import { Layout, Menu, Typography, Grid, Drawer, message, Switch } from 'antd';
 import {
   AppstoreOutlined,
   BellOutlined,
@@ -9,9 +11,13 @@ import {
   CloseOutlined,
 } from '@ant-design/icons';
 import Avatar from '../../Components/Avatar';
+import Button from '../../Components/Button';
 import Profile from './Profile';
 import Orders from './Orders';
 import Notifications from './Notifications';
+import { AuthContext } from '../../Context/Authentication';
+import { HOME_PAGE } from '../../Utils/routes.constant';
+
 import './style.css';
 
 const { Sider, Content } = Layout;
@@ -22,6 +28,9 @@ const DashboardProvider = () => {
   const [visible, setVisible] = useState(false);
   const [page, setPage] = useState(<Orders />);
   const [title, setTitle] = useState('Orders');
+
+  const { setRefresh, refresh, setAuthLoading } = useContext(AuthContext);
+  const history = useHistory();
 
   const showDrawer = () => {
     setVisible(true);
@@ -42,23 +51,53 @@ const DashboardProvider = () => {
       setTitle('Profile');
     }
   };
+  const handleClick = async () => {
+    try {
+      await Axios('/api/v1/logout');
+      setRefresh(!refresh);
+
+      setAuthLoading(true);
+      history.push(HOME_PAGE);
+    } catch (err) {
+      message.error('Something went wrong!');
+    }
+  };
+  function onChange(checked) {
+    console.log(`switch to ${checked}`);
+  }
   const mySider = (
-    <Sider>
-      <div className="logo">
-        <Avatar size={100} />
-        <Text>name</Text>
+    <Sider className="siderStyle">
+      <div>
+        <div className="logo">
+          <Avatar size={100} />
+          <Text>name</Text>
+        </div>
+        <Menu
+          onClick={handleChangMenu}
+          mode="inline"
+          defaultSelectedKeys={['1']}
+        >
+          <Menu.Item key="1" icon={<AppstoreOutlined />}>
+            Orders
+          </Menu.Item>
+          <Menu.Item key="2" icon={<BellOutlined />}>
+            Notifications
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UserOutlined />}>
+            Profile
+          </Menu.Item>
+        </Menu>
       </div>
-      <Menu onClick={handleChangMenu} mode="inline" defaultSelectedKeys={['1']}>
-        <Menu.Item key="1" icon={<AppstoreOutlined />}>
-          Orders
-        </Menu.Item>
-        <Menu.Item key="2" icon={<BellOutlined />}>
-          Notifications
-        </Menu.Item>
-        <Menu.Item key="3" icon={<UserOutlined />}>
-          Profile
-        </Menu.Item>
-      </Menu>
+
+      <div>
+        <div className="available">
+          <span> Available ?</span>
+          <Switch onChange={onChange} />
+        </div>
+        <Button className="fourthButton initial-style" onClick={handleClick}>
+          LogOut
+        </Button>
+      </div>
     </Sider>
   );
   return (
@@ -83,7 +122,9 @@ const DashboardProvider = () => {
               </>
             ) : null}
             <Text>{title}</Text>
-            <BellFilled />
+            <div className="bell">
+              <BellFilled />
+            </div>
           </div>
           {page}
         </Content>
