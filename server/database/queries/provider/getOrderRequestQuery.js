79d008c0bd5b8710743920df98be15e37cb936ef
Muhammad Alabadsa <moh22389@gmail.com>
2021-04-07 12:36:45 +0300

    Make change on the query to get user data Relates #37

--- server/database/queries/provider/getOrderRequestQuery.js
+++ server/database/queries/provider/getOrderRequestQuery.js
@@ -2,8 +2,11 @@ const connection = require('../../config/connection');
 
 const getOrderRequestQuery = (providerId) => {
   const sql = {
-    text:
-      'SELECT description, state, date FROM ordes_request WHERE provider_id = $1;',
+    text: `SELECT orequest.id, orequest.provider_id, u.username, u.avatar, u.location, u.mobile, orequest.description, 
+      orequest.date, orequest.state 
+      FROM orders_request AS orequest 
+      INNER JOIN users AS u ON orequest.user_id = u.id 
+      WHERE orequest.provider_id = $1;`,
     values: [providerId],
   };
   return connection.query(sql);
