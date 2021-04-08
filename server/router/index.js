586a8b0361106ad6ebb853e1412b0225f1b0df49
zeinjendeya <z.jendeya@gmail.com>
2021-04-08 15:14:14 +0300

    added auth provider routes middleware in router index

--- server/router/index.js
+++ server/router/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 
-const { protectMiddleware } = require('../middleware/auth');
+const {
+  protectMiddleware,
+  providerProtectMiddleware,
+} = require('../middleware/auth');
 const publicRoutes = require('./publicRoutes');
 const user = require('./user');
 const provider = require('./provider');
@@ -10,6 +13,7 @@ router.use(publicRoutes);
 // protect endpoints
 router.use(protectMiddleware);
 router.use(user);
+router.use(providerProtectMiddleware);
 router.use(provider);
 
 module.exports = router;
