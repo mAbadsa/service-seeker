d785ee81929362a9fe6ab5ba37dce322be1d9d39
zeinjendeya <z.jendeya@gmail.com>
2021-04-08 13:14:15 +0300

    query to get all accepted orders for the provider

---
+++ server/database/queries/provider/getAcceptOrders.js
@@ -0,0 +1,19 @@
+const connection = require('../../config/connection');
+
+const getAcceptedOrders = (providerID) => {
+  const sql = {
+    text:
+      "SELECT users.avatar, users.username, users.mobile, users.location, orders_request.description, orders_request.date, orders.state FROM orders INNER JOIN orders_request ON orders.orders_request_id = orders_request.id INNER JOIN users ON orders_request.user_id = users.id WHERE orders_request.state = 'accepted' AND orders_request.provider_id = $1;",
+    values: [providerID],
+  };
+
+  return connection.query(sql);
+};
+
+module.exports = getAcceptedOrders;
+
+// SELECT users.avatar, users.username, users.mobile, users.location, orders_request.description,
+// orders_request.date, orders.state
+// FROM orders INNER JOIN orders_request ON orders.orders_request_id = orders_request.id
+// INNER JOIN users ON orders_request.user_id = users.id WHERE orders_request.state = 'accepted'
+// AND orders_request.provider_id = 1;
