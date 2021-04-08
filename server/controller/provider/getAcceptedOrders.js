e1f3f090967fe40c468cffcee34d911fd94de7fd
zeinjendeya <z.jendeya@gmail.com>
2021-04-08 13:23:31 +0300

    get accepted orders controller

---
+++ server/controller/provider/getAcceptedOrders.js
@@ -0,0 +1,14 @@
+const { getAcceptedOrders } = require('../../database/queries');
+
+const getAcceptedOrdersController = async (req, res, next) => {
+  const { id: providerID } = req.user;
+  try {
+    const { rows } = await getAcceptedOrders(providerID);
+
+    res.json({ statusCode: 200, data: rows });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = getAcceptedOrdersController;
