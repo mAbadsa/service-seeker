b4ba0dbcec8954665bb84b480a2f3169de33676e
zeinjendeya <z.jendeya@gmail.com>
2021-04-08 13:36:27 +0300

    fixed bug if user sent the request

--- server/controller/provider/getAcceptedOrders.js
+++ server/controller/provider/getAcceptedOrders.js
@@ -1,8 +1,14 @@
 const { getAcceptedOrders } = require('../../database/queries');
+const { boomify } = require('../../utils');
 
 const getAcceptedOrdersController = async (req, res, next) => {
-  const { id: providerID } = req.user;
+  const { id: providerID, role } = req.user;
+
   try {
+    if (role === 'user') {
+      throw boomify(401, 'unauthorized');
+    }
+
     const { rows } = await getAcceptedOrders(providerID);
 
     res.json({ statusCode: 200, data: rows });
