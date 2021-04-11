1e6748960dc4848988f0b3c9888a8f7aad4f8584
zeinjendeya <z.jendeya@gmail.com>
2021-04-11 17:05:54 +0300

    fixed a bug in getAcceptedOrders route controller

--- server/controller/provider/getAcceptedOrders.js
+++ server/controller/provider/getAcceptedOrders.js
@@ -1,12 +1,15 @@
-const { getAcceptedOrders } = require('../../database/queries');
+const {
+  getAcceptedOrders,
+  getProviderDataById,
+} = require('../../database/queries');
 
 const getAcceptedOrdersController = async (req, res, next) => {
   const { id: providerID } = req.user;
-  console.log(providerID);
 
   try {
-    const { rows } = await getAcceptedOrders(providerID);
-    console.log(rows);
+    const { rows: provider } = await getProviderDataById({ id: providerID });
+
+    const { rows } = await getAcceptedOrders(provider[0].id);
 
     res.json({ statusCode: 200, data: rows });
   } catch (err) {
