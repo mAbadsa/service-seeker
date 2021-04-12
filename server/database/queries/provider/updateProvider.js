c3aef42919723ee7ad6bab21c3a471305407ea80
Ahmed-aed <aeid767@gmail.com>
2021-04-12 19:03:19 +0300

    fix name of file

--- server/database/queries/provider/updateProvider.js
+++ server/database/queries/provider/updateProvider.js
@@ -13,18 +13,17 @@ const updateProfileProviders = async ({
 }) => {
   try {
     await connection.query('BEGIN TRANSACTION');
-    const updateProvider = await connection.query({
+    await connection.query({
       text:
         'UPDATE providers SET title=$1,bio=$2,price_hour=$3,cover_image=$4,service_type=$5 WHERE user_id=$6 ;',
       values: [title, bio, priceHour, coverImage, serviceType, id],
     });
-    const updateUser = await connection.query({
+    await connection.query({
       text: 'UPDATE users set location=$1 ,mobile=$2 ,avatar=$3 WHERE id=$4 ;',
       values: [location, mobile, avatar, id],
     });
 
     await connection.query('COMMIT');
-    return { updateProvider, updateUser };
   } catch (e) {
     await connection.query('ROLLBACK');
     throw e;
