55531e23a0f54edb2f018251997b584a5df97b81
zeinjendeya <z.jendeya@gmail.com>
2021-04-13 11:23:00 +0300

    returned route as it was

--- server/controller/orders/getPendingOrderRequest.js
+++ server/controller/orders/getPendingOrderRequest.js
@@ -1,13 +1,9 @@
-const {
-  getPendingOrderRequest,
-  getProviderDataById,
-} = require('../../database/queries');
+const { getPendingOrderRequest } = require('../../database/queries');
 
 const getPendingOrderRequestController = async (req, res, next) => {
   try {
     const { id: providerId } = req.user;
-    const { rows: provider } = await getProviderDataById({ id: providerId });
-    const { rows } = await getPendingOrderRequest(provider[0].id);
+    const { rows } = await getPendingOrderRequest(providerId);
 
     res.json({
       statusCode: 200,
