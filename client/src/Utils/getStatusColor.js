eca48736e11f7a4b1c9882af53803644ad865837
zeinjendeya <z.jendeya@gmail.com>
2021-04-13 13:24:39 +0300

    modefied getStateColor to fit the states we have

--- client/src/Utils/getStatusColor.js
+++ client/src/Utils/getStatusColor.js
@@ -2,10 +2,12 @@ const getStatusColor = (text) => {
   switch (text) {
     case 'Didn’t start':
       return '#F97272';
-    case 'Finished':
+    case 'finished':
       return '#13E842';
-    case 'Pause':
+    case 'pause':
       return '#332A94';
+    case 'accepted':
+      return '#ffa812';
     default:
       return '#5C5C5C';
   }
