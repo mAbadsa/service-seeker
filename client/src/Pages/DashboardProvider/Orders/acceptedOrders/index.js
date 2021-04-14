b97fe383aa60af1c72497735b8ae90635feee0c7
Muhammad Alabadsa <moh22389@gmail.com>
2021-04-14 12:18:59 +0300

    Add StartWorkModal on acceptOrder Relates #136

--- client/src/Pages/DashboardProvider/Orders/acceptedOrders/index.js
+++ client/src/Pages/DashboardProvider/Orders/acceptedOrders/index.js
@@ -5,10 +5,13 @@ import Axios from 'axios';
 import { Alert } from 'antd';
 
 import Table from '../../../../Components/Table';
+import StartWorkModal from './StartWorkModal';
 
 const AcceptedOrders = ({ refresh }) => {
   const [acceptedOrders, setAcceptedOrders] = useState();
   const [isLoading, setIsLoading] = useState(false);
+  const [orderDetails, setOrderDetails] = useState(null);
+  const [showModal, setShowModal] = useState(false);
   const [errMsg, setErrMsg] = useState(null);
 
   useEffect(() => {
@@ -33,33 +36,63 @@ const AcceptedOrders = ({ refresh }) => {
     };
   }, [refresh]);
 
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
+  const handleMoreDetails = (_1, _2, record) => {
+    setOrderDetails(record);
+    handleShowModal();
+  };
+
   return errMsg ? (
     <Alert type="error" message={errMsg} />
   ) : (
-    <Table
-      loading={isLoading}
-      ColumnsType="providerAcceptedOrders"
-      dataSource={acceptedOrders?.map(
-        ({
-          username,
-          avatar,
-          location,
-          mobile: phone,
-          state: status,
-          description,
-          id,
-          date,
-        }) => ({
-          userinfo: [username, avatar],
-          location,
-          phone,
-          status,
-          description,
-          key: id,
-          time: date,
-        })
+    <>
+      {orderDetails && (
+        <StartWorkModal
+          data={{
+            username: orderDetails.userinfo[0],
+            location: orderDetails.location,
+            date: orderDetails.time,
+            description: orderDetails.description,
+            mobile: orderDetails.phone,
+            avatar: orderDetails.userinfo[1],
+          }}
+          showModal={showModal}
+          onCancel={handleCloseModal}
+        />
       )}
-    />
+      <Table
+        loading={isLoading}
+        ColumnsType="providerAcceptedOrders"
+        dataSource={acceptedOrders?.map(
+          ({
+            username,
+            avatar,
+            location,
+            mobile: phone,
+            state: status,
+            description,
+            id,
+            date,
+          }) => ({
+            userinfo: [username, avatar],
+            location,
+            phone,
+            status,
+            description,
+            key: id,
+            time: date,
+          })
+        )}
+        onRowDoubleClick={handleMoreDetails}
+      />
+    </>
   );
 };
 
