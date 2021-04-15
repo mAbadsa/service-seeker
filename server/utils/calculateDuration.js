ce58da92d52b78827766ffaf645b9a208149a993
zeinjendeya <z.jendeya@gmail.com>
2021-04-16 01:35:25 +0300

    restructured calculte duration

--- server/utils/calculateDuration.js
+++ server/utils/calculateDuration.js
@@ -1,6 +1,7 @@
-/* eslint-disable prettier/prettier */
-const moment = require('moment');
-
-const calculateDuration = (end, start) => moment.duration(end.diff(start.add(3, 'hours'))).asHours();
+const calculateDuration = (dt1, dt2) => {
+  let diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60 * 60;
+  return Math.abs(Math.round(diff));
+};
 
 module.exports = calculateDuration;
