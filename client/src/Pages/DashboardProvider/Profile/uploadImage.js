8aa6cfa36238bf45c7fcac3be49da74a99230d29
mariamisa <maryaam1998@hotmail.com>
2021-04-20 00:46:56 +0300

    add

--- client/src/Pages/DashboardProvider/Profile/uploadImage.js
+++ client/src/Pages/DashboardProvider/Profile/uploadImage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Axios from 'axios';
 import { Upload, message } from 'antd';
@@ -12,36 +12,39 @@ function UserAvatar({ image, setRefresh }) {
   const [loading, setLoading] = useState(false);
   const [selectedFile, setSelectedFile] = useState(image);
 
-  const beforeUpload = (file) => {
+  const onChange = (file) => {
     const isLt2M = file.size / 1024 / 1024 < 2;
     if (!isLt2M) {
       message.error('Image must smaller than 2MB!');
     }
     setSelectedFile(file);
   };
-  const onChange = async () => {
-    try {
-      setLoading(true);
-      const formData = new FormData();
-      formData.append('coverImage', selectedFile);
-      await Axios.patch('/api/v1/provider/cover-image', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      setLoading(false);
-      setRefresh();
-      message.destroy();
-      message.success('Image uploaded successfully');
-    } catch (err) {
-      setLoading(false);
-      message.error(err.response.data.message || 'Something went wrong!');
-    }
-  };
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        const formData = new FormData();
+        formData.append('coverImage', selectedFile);
+        await Axios.patch('/api/v1/provider/cover-image', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+        console.log(111111111);
+        setLoading(false);
+        setRefresh();
+        message.destroy();
+        message.success('Image uploaded successfully');
+      } catch (err) {
+        setLoading(false);
+        message.error(err.response.data.message || 'Something went wrong!');
+      }
+    })();
+  }, [selectedFile]);
+
   return (
     <Upload
       action="/api/v1/provider/cover-image"
-      beforeUpload={beforeUpload}
       onChange={onChange}
       showUploadList={false}
     >
