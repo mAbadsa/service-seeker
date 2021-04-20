1abe15cebe3faf8ce8a7ee3325f08c9ec2458883
Osama-you <osama.w.younis@gmail.com>
2021-04-20 13:21:06 +0300

    add info

--- client/src/Utils/moreInfo.constant.js
+++ client/src/Utils/moreInfo.constant.js
@@ -1,2 +1,5 @@
-export const USER_ORDER = '';
-export const DASHBOARD_ORDER = '';
+/* eslint-disable operator-linebreak */
+export const USER_ORDER =
+  'On this page, you can see all your requests so that you can see the pending requests from the Orders Request tab, and you can see the accepted requests and its status from the Orders Tab.';
+export const DASHBOARD_ORDER =
+  'On this page, you can see all requests customers so that you can see the pending requests from the Orders Request tab, and you can see the accepted requests and update its status from the Orders Tab.';
