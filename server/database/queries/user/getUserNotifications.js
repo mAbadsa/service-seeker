66534dff8276b176a7d0648fe72cde0e331a4773
Muhammad Alabadsa <moh22389@gmail.com>
2021-04-20 18:23:01 +0300

    Return user notification with order by desc Relates #178

--- server/database/queries/user/getUserNotifications.js
+++ server/database/queries/user/getUserNotifications.js
@@ -3,7 +3,7 @@ const connection = require('../../config/connection');
 const getUserNotifications = ({ userId }) => {
   const sql = {
     text:
-      'SELECT id, description, created_at FROM notification WHERE user_id = $1;',
+      'SELECT id, description, created_at FROM notification WHERE user_id = $1 ORDER BY id DESC;',
     values: [userId],
   };
   return connection.query(sql);
