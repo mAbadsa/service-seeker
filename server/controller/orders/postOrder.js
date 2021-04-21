4f86d51c920d023b4e237815ea113f4a8013dc2a
Osama-you <osama.w.younis@gmail.com>
2021-04-21 14:13:11 +0300

    hundle the notification in accept order, and updateOrderState

--- server/controller/orders/postOrder.js
+++ server/controller/orders/postOrder.js
@@ -3,18 +3,20 @@ const {
   updateStateOrderRequest,
 } = require('../../database/queries');
 
-const { boomify } = require('../../utils');
+const { boomify, sendNotification } = require('../../utils');
 
 const postOrderController = async (req, res, next) => {
   const { id: userID } = req.user;
   const { arriveTime, orderID } = req.body;
 
   try {
-    const { rowCount } = await updateStateOrderRequest(orderID, userID);
+    const { rows, rowCount } = await updateStateOrderRequest(orderID, userID);
     if (rowCount === 0) {
       throw boomify(409, 'failed to accept the order');
     }
     await postOrder(arriveTime, orderID);
+
+    sendNotification(userID, rows[0].user_id, 'acceptOrderReq');
 
     res.status(201).json({
       status: 201,
